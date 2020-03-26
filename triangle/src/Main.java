a429f7b8bdcc1fe5b437ebe1c074dfbcc2b58ed9
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-03-26 17:47:33 +0700

    Add files via upload

---
+++ triangle/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(5,10,12);
+        System.out.println(triangle);
+        triangle.setColor("blue");
+        System.out.println(triangle);
+    }
+}
